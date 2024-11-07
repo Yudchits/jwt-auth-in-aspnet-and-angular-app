411e4872a54328efad3b3c17b9546605a17d92a6
Yudchits <pashka.yudchits@gmail.com>
2024-11-07 20:08:22 +0300

    API: Added db context

---
+++ JwtAuthenticationAPI/JwtAuthentication.DataAccess/Context/JwtAuthenticationContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace JwtAuthentication.DataAccess.Context
+{
+    public class JwtAuthenticationContext : DbContext
+    {
+        public JwtAuthenticationContext(DbContextOptions<JwtAuthenticationContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
