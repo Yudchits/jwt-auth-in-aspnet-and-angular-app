9bd62d38c0806aa613db2cab3c4256ddd374cc73
Yudchits <pashka.yudchits@gmail.com>
2024-11-08 13:59:52 +0300

    Resource API: Added db context

---
+++ MarketplaceAPI/Marketplace.DataAccess/Context/MarketplaceContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Marketplace.DataAccess.Context
+{
+    public class MarketplaceContext : DbContext
+    {
+        public MarketplaceContext(DbContextOptions<MarketplaceContext> options) : base(options)
+        {
+        }
+    }
+}
