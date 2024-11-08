79e951667638d4736e81d4b010456c7e708f60b6
Yudchits <pashka.yudchits@gmail.com>
2024-11-08 14:10:41 +0300

    Resource API: Migration 'AddedCarTable'

---
+++ MarketplaceAPI/Marketplace.DataAccess/Migrations/20241108110951_AddedCarTable.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Marketplace.DataAccess.Migrations
+{
+    public partial class AddedCarTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Brand = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false),
+                    Model = table.Column<string>(type: "nvarchar(64)", maxLength: 64, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cars");
+        }
+    }
+}
